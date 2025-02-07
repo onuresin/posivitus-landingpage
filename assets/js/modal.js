0528ff6b2a2d445225b5b661ecd1c1a7ea38e6d7
Onur Esin <onuresn034@gmail.com>
2025-02-07 22:16:22 +0300

    modal ve hamburger menu sorunlari giderildi

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -4,15 +4,19 @@ document.addEventListener("DOMContentLoaded", function () {
     const menuModal = document.querySelector('.menu-modal');
 
     menuBtn.addEventListener('click', function() {
-        menuModal.style.display = 'flex';
+        menuBtn.classList.toggle('active');
+        menuModal.style.display = menuModal.style.display === 'flex' ? 'none' : 'flex';
     });
 
     closeBtn.addEventListener('click', function(){
        menuModal.style.display = 'none';
+       menuBtn.classList.remove('active');
     });
-    menuModal.addEventListener('click', function(event){
-        if(event.target === menuModal) {
-            display = 'none';
-        }});
+    menuModal.addEventListener("click", function (event) {
+        if (event.target === menuModal) {
+            menuModal.style.display = "none";
+            menuBtn.classList.remove("active");
+        }
+    });
 }
 )
